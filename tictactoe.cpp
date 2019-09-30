f59296398e9570adaa3c0fa8d8e11740cf3134c0
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-30 10:03:22 -0700

    failed use of switch statement

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -10,6 +10,7 @@ void prtGrid(char grid[3][3]);
 char setGrid(char gridSpace);
 char changePlayer(char turn);
 bool checkInput(char input[2]);
+void updateGrid(char input[2]);
 
 int main() {
   cout << "Welcome to Tic Tac Toe!" << endl;
@@ -25,7 +26,7 @@ int main() {
   bool isPlaying = true;
   while(isPlaying) {
     cout << "it's " << turn << "'s turn!";
-    cin.get(play, 2);
+    cin.get(play, 3);
     cin.get();
     isPlaying = checkInput(play);
   }
@@ -63,7 +64,8 @@ char changePlayer(char turn) {
 bool checkInput(char input[2]) {
   if((input[0] > 64 && input[0] < 68) || (input[0] > 96 && input[0] < 100)) {
     if(input[1] > 48 && input[1] < 52) {
-      
+      updateGrid(input);
+      return true;
     }
     else {
       cout << "invalid collumn" << endl;
@@ -74,4 +76,51 @@ bool checkInput(char input[2]) {
     cout << "invalid row" << endl;
     return false;
   }
+}
+
+void updateGrid(char input[2]) {
+  int row = -1;
+  int collumn = -1;
+  switch (input[0]) {
+  case 65: row = 0;
+    switch (input[1]) {
+    case 1:
+      collumn = 0;
+      break;
+    case 2:
+      collumn = 1;
+      break;
+    case 3:
+      collumn = 2;
+      break;
+    }
+    break;
+  case 66: row = 1;
+    switch (input[1]) {
+    case 1:
+      column = 0;
+      break;
+    case 2:
+      column = 1;
+      break;
+    case 3:
+      column = 2;
+      break;
+    }
+    break;
+  case 67: row = 2;
+    switch (input[1]) {
+    case 1:
+      column = 0;
+      break;
+    case 2:
+      column = 1;
+      break;
+    case 3:
+      column = 2;
+      break;
+    }
+    break;
+  }
+  return;
 }
