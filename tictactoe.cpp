69c5459c1be1988f3ffa43cabbb1047e35e5aec8
U-BSD\321341 <321341@SHS-10DECBC5A.beaverton.k12.or.us>
2019-09-30 10:47:50 -0700

    can now make plays

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -10,25 +10,44 @@ void prtGrid(char grid[3][3]);
 char setGrid(char gridSpace);
 char changePlayer(char turn);
 bool checkInput(char input[2]);
-void updateGrid(char input[2]);
+void updateGrid(char * row, char * collumn);
+int getRow(char inputRow);
+int getColumn(char inputColumn);
 
 int main() {
   cout << "Welcome to Tic Tac Toe!" << endl;
   char grid[3][3];
   char play[2];
-  char turn = 'x';
+  char turn = 'X';
   for(int i = 0; i < 3; i++) {
     for(int j = 0; j < 3; j++) {
       grid[i][j] = setGrid(grid[i][j]);
     }
-  }
+  } 
   prtGrid(grid);
   bool isPlaying = true;
   while(isPlaying) {
+    int row = -1;
+    int column = -1;
     cout << "it's " << turn << "'s turn!";
     cin.get(play, 3);
     cin.get();
-    isPlaying = checkInput(play);
+    row = getRow(play[0]);
+    column = getColumn(play[1]);
+    grid[row][column] = turn;
+    if(turn == 'X') {
+      turn = 'O';
+    }
+    else {
+      turn = 'X';
+    }
+    for(int i = 0; i < 3; i++) {
+      for(int j = 0; j < 3; j++) {
+	cout << grid[i][j] << " ";
+      }
+      cout << endl;
+    }
+    prtGrid(grid);
   }
   return 0;
 }
@@ -61,10 +80,10 @@ char changePlayer(char turn) {
   return turn;
 }
 
-bool checkInput(char input[2]) {
+bool checkInput(char input[2], char * gridPtr) {
   if((input[0] > 64 && input[0] < 68) || (input[0] > 96 && input[0] < 100)) {
     if(input[1] > 48 && input[1] < 52) {
-      updateGrid(input);
+      //changeRow(input[0]);
       return true;
     }
     else {
@@ -78,49 +97,30 @@ bool checkInput(char input[2]) {
   }
 }
 
-void updateGrid(char input[2]) {
-  int row = -1;
-  int collumn = -1;
-  switch (input[0]) {
-  case 65: row = 0;
-    switch (input[1]) {
-    case 1:
-      collumn = 0;
-      break;
-    case 2:
-      collumn = 1;
-      break;
-    case 3:
-      collumn = 2;
-      break;
-    }
-    break;
-  case 66: row = 1;
-    switch (input[1]) {
-    case 1:
-      column = 0;
-      break;
-    case 2:
-      column = 1;
-      break;
-    case 3:
-      column = 2;
-      break;
-    }
-    break;
-  case 67: row = 2;
-    switch (input[1]) {
-    case 1:
-      column = 0;
-      break;
-    case 2:
-      column = 1;
-      break;
-    case 3:
-      column = 2;
-      break;
-    }
-    break;
+int getRow(char rowInput) {
+  if(rowInput == 'A' || rowInput == 'a') {
+    return 0;
   }
-  return;
+  else if (rowInput == 'B' || rowInput == 'b') {
+    return 1;
+  }
+  else if (rowInput == 'C' || rowInput == 'c') {
+    return 2;
+  }
+}
+
+int getColumn(char columnInput) {
+  if(columnInput == '1') {
+    return 0;
+  }
+  else if(columnInput == '2') {
+    return 1;
+  }
+  else if(columnInput == '3') {
+    return 2;
+  }
+}
+
+char updateGrid(int row, int column, char turn) {
+  
 }
